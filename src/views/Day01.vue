64566390e7b8f8e6efdd5445444f40f98e3bec04
Tim Hsu <tim.hsu@tpisoftware.com>
2023-05-16 23:24:21 +0800

    day01 update

--- src/views/Day01.vue
+++ src/views/Day01.vue
@@ -1,54 +1,92 @@
 <template>
   <div>
     <h2 class="my-4 text-2xl font-bold">下拉選單與搜尋對應列表</h2>
-    <div class="w-1/3">
-      <h3 class="capitalize my-4">forEach select</h3>
-      <select
-        v-model="forEachSelectedValue"
-        class="w-full p-2 border border-gray-300 rounded-md"
-        @change="getTravelDataByForEach($event)"
-      >
-        <option value="">---請選擇---</option>
-        <option
-          v-for="item in travelList"
-          :key="item.id"
-          :value="item.area"
-          :id="item.id"
+    <div class="grid lg:grid-cols-2 gap-2">
+      <div class="w-full">
+        <h3 class="capitalize my-4">forEach select</h3>
+        <select
+          v-model="forEachSelectedValue"
+          class="w-full p-2 border border-gray-300 rounded-md"
+          @change="getTravelDataByForEach($event)"
         >
-          {{ item.area }}
-        </option>
-      </select>
+          <option value="">---請選擇---</option>
+          <option
+            v-for="item in travelList"
+            :key="item.id"
+            :value="item.area"
+            :id="item.id"
+          >
+            {{ item.area }}
+          </option>
+        </select>
 
-      <div
-        class="rounded-md overflow-hidden object-cover flex justify-center items-center"
-      >
-        <template v-for="item in travelList" :key="item.id">
-          <img
-            v-if="item.area === forEachSelectedValue"
-            :src="item.imgUrl"
-            :alt="`pic-${item.area}`"
-            class="mt-4 shadow"
-          />
-        </template>
+        <div
+          class="rounded-md overflow-hidden object-cover flex justify-center items-center"
+        >
+          <template v-for="item in travelList" :key="item.id">
+            <div
+              class="w-full lg:flex lg:items-start"
+              v-if="item.area === forEachSelectedValue"
+            >
+              <img
+                :src="item.imgUrl"
+                :alt="`pic-${item.area}`"
+                class="mt-4 shadow w-full lg:w-1/2"
+              />
+              <div class="ml-4 mt-4">
+                <h3 class="text-amber-700 text-2xl font-bold">
+                  {{ item.name }}
+                </h3>
+                <p class="text-gray-600">地點：{{ item.area }}</p>
+                <p class="text-gray-600">介紹：{{ item.description }}</p>
+              </div>
+            </div>
+          </template>
+        </div>
       </div>
-    </div>
 
-    <div class="w-1/3 mt-10">
-      <h3 class="capitalize my-4">filter select</h3>
-      <select
-        v-model="filterSelectedValue"
-        class="w-full p-2 border border-gray-300 rounded-md"
-      >
-        <option value="">---請選擇---</option>
-        <option
-          v-for="item in travelList"
-          :key="item.id"
-          :value="item.area"
-          :id="item.id"
+      <div class="w-full">
+        <h3 class="capitalize my-4">filter select</h3>
+        <select
+          v-model="filterSelectedValue"
+          class="w-full p-2 border border-gray-300 rounded-md"
+          @change="getTravelDataByFilter($event)"
         >
-          {{ item.area }}
-        </option>
-      </select>
+          <option value="">---請選擇---</option>
+          <option
+            v-for="item in travelList"
+            :key="item.id"
+            :value="item.area"
+            :id="item.id"
+          >
+            {{ item.area }}
+          </option>
+        </select>
+
+        <div
+          class="rounded-md overflow-hidden object-cover flex justify-center items-center"
+        >
+          <template v-for="item in travelList" :key="item.id">
+            <div
+              class="w-full lg:flex lg:items-start"
+              v-if="item.area === filterSelectedValue"
+            >
+              <img
+                :src="item.imgUrl"
+                :alt="`pic-${item.area}`"
+                class="mt-4 shadow w-full lg:w-1/2"
+              />
+              <div class="ml-4 mt-4">
+                <h3 class="text-blue-700 text-2xl font-bold">
+                  {{ item.name }}
+                </h3>
+                <p class="text-gray-600">地點：{{ item.area }}</p>
+                <p class="text-gray-600">介紹：{{ item.description }}</p>
+              </div>
+            </div>
+          </template>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -67,22 +105,8 @@ export default {
       getImgByForEach: "",
     };
   },
-  watch: {
-    forEachSelectedValue(value) {
-      this.forEachSelectedValue = value;
-    },
-    filterSelectedValue(value) {
-      this.filterSelectedValue = value;
-    },
-  },
   created() {
     this.getTravelData();
-    if (this.travelList.length > 0) {
-      this.forEachSelectedValue = this.travelList[0].area;
-      this.getTravelDataByForEach({
-        target: { value: this.forEachSelectedValue },
-      });
-    }
   },
   methods: {
     getTravelData() {
@@ -95,8 +119,14 @@ export default {
       this.travelList.forEach((item) => {
         if (item.area === value) {
           this.getImgByForEach = item.imgUrl;
-        } else if (value === "") {
-          this.getImgByForEach = "";
+        }
+      });
+    },
+    getTravelDataByFilter(e) {
+      const value = e.target.value;
+      this.travelList.filter((item) => {
+        if (item.area === value) {
+          this.getImgByForEach = item.imgUrl;
         }
       });
     },
